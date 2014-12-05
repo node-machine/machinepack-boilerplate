a1e577705b3a25f8a0daf76422afd3b261b0a45d
Irl Nathan <irlnathan@gmail.com>
2014-12-05 10:10:56 -0600

    added functionality to helloworld

--- machines/hello-world.js
+++ machines/hello-world.js
@@ -1,33 +1,54 @@
 module.exports = {
 
   friendlyName: 'Hello World',
-  description: 'This is an initial machine that you should replace with your own machine',
-  extendedDescription: 'This example machine ... ',
-  cacheable: true,
+  description: 'Log a hello message with the a user\'s secret code and return the length of the name input and the secret code.',
+  extendedDescription: 'This example machine is part of machinepack-boilerplate, used to introduce everyone to machines.',
 
   inputs: {
-    foo: {
-      example: 'baz',
-      description: '',
+
+    name: {
+      example: 'John',
+      description: 'The name of the person that will be sent the hello message.',
       required: true
     }
+
   },
 
   defaultExit: 'success',
   catchallExit: 'error',
 
   exits: {
+
     error: {
-      description: ''
+      description: '',
+      void: true
     },
+
     success: {
       description: '',
-      example: {
-        yaya: ''
+      example:  {
+        numLettersInName: 4,
+        secretCode: "e9ec627220bc9e8ca66f916b7fba92f3" 
+      }
     }
+
   },
 
   fn: function (inputs, exits) {
 
-  }  
+    var Hat = require('hat');
+
+    var secretCode = Hat();
+
+    var myLength = inputs.name.length;
+
+    console.log("Hello %s, your secret code is %s", inputs.name, secretCode); 
+
+    return exits.success({
+      numLettersInName: myLength, 
+      secretCode: secretCode
+    });
+
+  }
+
 };
