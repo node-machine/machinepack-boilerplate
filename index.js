36c1a16e26d5b8f95cdef83e8ed5d6378ede8b4e
Mike McNeil <mike@balderdash.co>
2014-12-08 10:49:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Export Machine.pack() object for convenience
+// This is a boilerplate file which should not need to be changed.
 module.exports = require('machine').pack({
   pkg: require('./package.json'),
   dir: __dirname
